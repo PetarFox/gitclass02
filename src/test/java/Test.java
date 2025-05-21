011a0050953fda368b37ed82bd01a7d186dda816
PetarFox <petarfox.office@gmail.com>
2025-05-21 14:12:29 -0700

    forth commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
-        System.out.println("This is project version 3");
+        System.out.println("This is project version 4");
     }
 }
-//done with version 3
+//done with version four
